68b1ce70694cf4779eb679a79444dd4b3d28b797
danhub-co <danielcn@stcmalta.edu.mt>
2026-01-06 05:35:12 -0800

    Add files via upload

---
+++ api_src_main_Version2.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log(`API listening on http://0.0.0.0:${port}`);
+}
+bootstrap();
